14523a1322a2a5bfc2c36e1e7aca6d82c71e8470
Caleb Maxie <cam71805@gmail.com>
2026-03-12 20:34:23 -0500

    Update ai_query_engine.py

--- backend/ai_query_engine.py
+++ backend/ai_query_engine.py
@@ -1,31 +1,14 @@
 import os
 from backend.config import client
 
-platforms = {
-    "ChatGPT":"gpt-4o-mini",
-    "Gemini":"gpt-4o-mini",
-    "Copilot":"gpt-4o-mini"
-}
-
 def query_ai(product):
 
-    query = f"What are the most popular brands or products for {product}? Give a ranked list."
+    query = f"""What are the most popular brands or products for {product}? Give a ranked list."""
 
-    results = {}
-
-    for platform, model in platforms.items():
-
-        try:
 
             response = client.chat.completions.create(
-                model="llama3-8b-8192",
+                model="openai/gpt-oss-120b",
                 messages=[{"role":"user","content":query}]
             )
 
             results[platform] = response.choices[0].message.content
-
-        except Exception as e:
-
-            results[platform] = str(e)
-
-    return results
