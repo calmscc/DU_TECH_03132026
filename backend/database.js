37229b71e1640c909379d3833dd32331a377cc24
Caleb Maxie <cam71805@gmail.com>
2026-03-13 04:55:08 -0500

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
+const audits = []
 
-const supabase = createClient(
- process.env.SUPABASE_URL,
- process.env.SUPABASE_ANON_KEY
-);
+export function saveAudit(data){
 
-export default supabase;
+ audits.push(data)
+
+}
+
+export function getAudits(){
+
+ return audits
+
+}
